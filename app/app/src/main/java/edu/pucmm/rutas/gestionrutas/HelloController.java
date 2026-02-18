6227f0229c756e286096ecfe6527a21380c865ae
Jadiel García Núñez <jxgn0001@ce.pucmm.edu.do>
2026-02-18 18:46:09 -0400

    primer commit proyecto final

---
+++ app/app/src/main/java/edu/pucmm/rutas/gestionrutas/HelloController.java
@@ -0,0 +1,14 @@
+package edu.pucmm.rutas.gestionrutas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ app/app/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module edu.pucmm.rutas.gestionrutas {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens edu.pucmm.rutas.gestionrutas to javafx.fxml;
+    exports edu.pucmm.rutas.gestionrutas;
+}
