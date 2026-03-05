fd0684795314f2ac7d27ec45af16812c63b197ce
LuijoGift <LJPD0001@ce.pucmm.edu.do>
2026-03-05 11:50:28 -0300

    Algoritmo dijkstra y clase grafo

--- app/app/src/main/java/edu/pucmm/rutas/gestionrutas/algoritmos/Dijkstra.java
+++ app/app/src/main/java/edu/pucmm/rutas/gestionrutas/algoritmos/Dijkstra.java
@@ -1,4 +1,59 @@
 package edu.pucmm.rutas.gestionrutas.algoritmos;
 
+import edu.pucmm.rutas.gestionrutas.modelo.Grafo;
+import edu.pucmm.rutas.gestionrutas.modelo.Parada;
+import edu.pucmm.rutas.gestionrutas.modelo.Ruta;
+
+import java.util.*;
+
 public class Dijkstra {
+
+    public List<Parada> dijkstra(Parada origen, Parada destino, Grafo grafo) {
+
+        Map<Parada, Double> distancia = new HashMap<>();
+        Map<Parada, Parada> anterior = new HashMap<>();
+        Set<Parada> visitados = new HashSet<>();
+
+        for (Parada p : grafo.getParadas().values()) {
+            distancia.put(p, Double.MAX_VALUE);
+        }
+
+        distancia.put(origen, 0.0);
+
+        Parada actual = origen;
+
+        while (actual != null) {
+
+            visitados.add(actual);
+
+            for (Ruta r : actual.getRutasSalientes()) {
+                Parada vecino = r.getParadaDestino();
+                double nuevaDistancia =
+                        distancia.get(actual) + r.getDistancia();
+                if (nuevaDistancia < distancia.get(vecino)) {
+                    distancia.put(vecino, nuevaDistancia);
+                    anterior.put(vecino, actual);
+                }
+            }
+            actual = null;
+            double menor = Double.MAX_VALUE;
+            for (Parada p : distancia.keySet()) {
+
+                if (!visitados.contains(p) && distancia.get(p) < menor) {
+                    menor = distancia.get(p);
+                    actual = p;
+                }
+            }
+            if (actual != null && actual.equals(destino)) {
+                break;
+            }
+        }
+        List<Parada> camino = new ArrayList<>();
+        Parada paso = destino;
+        while (paso != null) {
+            camino.add(0, paso);
+            paso = anterior.get(paso);
+        }
+        return camino;
+    }
 }
--- app/app/src/main/java/edu/pucmm/rutas/gestionrutas/modelo/Grafo.java
+++ app/app/src/main/java/edu/pucmm/rutas/gestionrutas/modelo/Grafo.java
@@ -6,6 +6,10 @@ import java.util.Map;
 
 public class Grafo {
 
+    public Map<String, Parada> getParadas() {
+        return paradas;
+    }
+
     private Map<String, Parada> paradas;
     private Map<String, List<Ruta>> rutasPorParada;
 
@@ -13,4 +17,52 @@ public class Grafo {
         this.paradas = new HashMap<>();
         this.rutasPorParada = new HashMap<>();
     }
+    //AGREGAR
+    public void anadirParada(Parada paragregar) {
+        if(!this.paradas.containsKey(paragregar.getId())) {
+            this.paradas.put(paragregar.getId(), paragregar);
+        }
+    }
+
+    public void anadirRuta(Ruta ruta) {
+        Parada origen = ruta.getParadaOrigen();
+        Parada destino = ruta.getParadaDestino();
+        if (paradas.containsKey(origen.getId()) && paradas.containsKey(destino.getId())) {
+            if (!origen.getRutasSalientes().contains(ruta)) {
+                origen.getRutasSalientes().add(ruta);
+            }
+        }
+    }
+    //MODIFICAR
+    public void modificarRuta(Ruta rutaActualizada) {
+        Parada origen = rutaActualizada.getParadaOrigen();
+        List<Ruta> rutas = origen.getRutasSalientes();
+        for (int i = 0; i < rutas.size(); i++) {
+            if (rutaActualizada.equals(rutas.get(i))) {
+                rutas.set(i, rutaActualizada);
+                break;
+            }
+        }
+    }
+    public void modificarparada (Parada parada) {
+        this.paradas.put(parada.getId(), parada);
+    }
+    //ELIMINAR
+
+    public void eliminarRuta(Ruta ruta) {
+        ruta.getParadaOrigen().getRutasSalientes().remove(ruta);
+    }
+    public void eliminarParada(String idParada) {
+        for (Parada p : paradas.values()) {
+            List<Ruta> rutas = p.getRutasSalientes();
+            for (int i = 0; i < rutas.size(); i++) {
+                if (rutas.get(i).getParadaDestino().getId().equals(idParada)) {
+                    rutas.remove(i);
+                    i--;
+                }
+            }
+        }
+        paradas.remove(idParada);
+    }
+
 }
